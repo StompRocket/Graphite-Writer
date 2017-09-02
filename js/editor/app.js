30758617226dac0a7416f7c56fe64efd433c3a01
Rosho123 <Rosho123@users.noreply.github.com>
2017-09-02 13:19:43 -0700

    saving

--- js/editor/app.js
+++ js/editor/app.js
@@ -1,7 +1,17 @@
+function GetURLParameter (sParam) {
+  var sPageURL = window.location.search.substring(1)
+  var sURLVariables = sPageURL.split('&')
+  for (var i = 0; i < sURLVariables.length; i++) {
+    var sParameterName = sURLVariables[i].split('=')
+    if (sParameterName[0] == sParam) {
+      return sParameterName[1]
+    }
+  }
+}
+
 var toolbarOptions = [
   ['bold', 'italic', 'underline', 'strike'], // toggled buttons
   ['blockquote', 'code-block'],
-
   [{
     'header': 1
   }, {
@@ -45,6 +55,46 @@ var toolbarOptions = [
   ['clean'] // remove formatting button
 ]
 $(document).ready(function ($) {
+  firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+      var user = firebase.auth().currentUser
+
+      var name, email, photoUrl, uid, emailVerified
+
+      if (user != null) {
+        name = user.displayName
+        email = user.email
+        photoUrl = user.photoURL
+        emailVerified = user.emailVerified
+        uid = user.uid // The user's ID, unique to the Firebase project. Do NOT use
+        // this value to authenticate with your backend server, if
+        // you have one. Use User.getToken() instead.
+      }
+      const documentname = GetURLParameter('d')
+
+      function saveDocument (data) {
+        firebase.database().ref('users/' + uid + '/docs/' + documentname + '/').set({
+          data: data
+        })
+      }
+
+      function updateContents () {
+        firebase.database().ref('users/' + uid + '/docs/' + documentname + '/').once('value').then(function (snapshot) {
+          var data = snapshot.val().data
+
+          quill.setContents(data)
+        })
+      }
+      updateContents()
+      quill.update()
+      quill.on('text-change', function (delta, oldDelta, source) {
+        console.log('change')
+        var currentdocument = quill.getContents()
+        saveDocument(currentdocument)
+      })
+    }
+  })
+
   var quill = new Quill('#editor', {
     modules: {
       toolbar: toolbarOptions
