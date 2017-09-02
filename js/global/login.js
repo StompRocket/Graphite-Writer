eb6308168442a9cbd085bced9f9dae6d07b0d045
Rosho123 <Rosho123@users.noreply.github.com>
2017-09-02 12:39:03 -0700

    more login

--- js/global/login.js
+++ js/global/login.js
@@ -34,10 +34,37 @@ $(document).ready(function ($) {
   })
   firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
+      var user = firebase.auth().currentUser
+
+      var name, email, photoUrl, uid, emailVerified
+
+      if (user != null) {
+        name = user.displayName
+        email = user.email
+        photoUrl = user.photoURL
+        emailVerified = user.emailVerified
+        uid = user.uid // The user's ID, unique to the Firebase project. Do NOT use
+        // this value to authenticate with your backend server, if
+        // you have one. Use User.getToken() instead.
+      }
+      firebase.database().ref('/users/' + uid + '/userinfo/username').once('value').then(function (snapshot) {
+        if (!snapshot.val()) {
+          console.log('no snapshot')
+          var welcomedoc = firebase.database().ref('users/' + uid + '/docs/').push()
+          welcomedoc.set({
+            'data': '<h1>Welcome To Graphite</h1><p><br></p><p>If you have any questions please email <a href="mailto:support@graphitewriter.com">support@graphitewriter.com</a></p>'
+          })
+        }
+        firebase.database().ref('users/' + uid + '/userinfo/').set({
+          username: name,
+          email: email,
+          profile_picture: photoUrl
+        })
+      })
       $('#loginBtn').text('Logout')
     } else {
       $('#loginBtn').text('Login')
-      if (window.location.href.endsWith('/app') || window.location.href.endsWith('/editor')) {
+      if (window.location.href.indexOf('app') >= 0 || window.location.href.indexOf('edit') >= 0) {
         window.location.href = '/index'
       }
     }
