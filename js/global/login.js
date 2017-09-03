96cd9d96bb8c4ee1a43d6cae49e192b9dba38c4b
Rosho123 <Rosho123@users.noreply.github.com>
2017-09-02 18:05:08 -0700

    redirect to add

--- js/global/login.js
+++ js/global/login.js
@@ -63,22 +63,7 @@ $(document).ready(function ($) {
     } else {
       $('#loginBtn').text('Login')
       if (window.location.href.indexOf('app') >= 0 || window.location.href.indexOf('edit') >= 0) {
-        firebase.auth().signInWithPopup(provider).then(function (result) {
-          // This gives you a Google Access Token. You can use it to access the Google API.
-          var token = result.credential.accessToken
-          // The signed-in user info.
-          var user = result.user
-          // ...
-        }).catch(function (error) {
-          // Handle Errors here.
-          var errorCode = error.code
-          var errorMessage = error.message
-          // The email of the user's account used.
-          var email = error.email
-          // The firebase.auth.AuthCredential type that was used.
-          var credential = error.credential
-          // ...
-        })
+        firebase.auth().signInWithRedirect(provider)
       }
     }
   })
