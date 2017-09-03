8b7d6069dcbab029bccc3b6ec1017d88097acabe
Rosho123 <Rosho123@users.noreply.github.com>
2017-09-03 08:37:53 -0700

    type on

--- js/home/main.js
+++ js/home/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function ($) {
-  var str = 'This is a test',
+  var str = 'a modern text editor.t',
     i = 0,
     isTag,
     text;
@@ -17,4 +17,44 @@ $(document).ready(function ($) {
     if (isTag) return type()
     setTimeout(type, 80)
   }())
+  setTimeout(function () {
+    var str = 'a faster text editor.t',
+      i = 0,
+      isTag,
+      text;
+
+    (function type () {
+      text = str.slice(0, ++i)
+      if (text === str) return
+
+      document.getElementById('typewriter').innerHTML = text
+
+      var char = text.slice(-1)
+      if (char === '<') isTag = true
+      if (char === '>') isTag = false
+
+      if (isTag) return type()
+      setTimeout(type, 80)
+    }())
+    setTimeout(function () {
+      var str = 'an easier text editor.t',
+        i = 0,
+        isTag,
+        text;
+
+      (function type () {
+        text = str.slice(0, ++i)
+        if (text === str) return
+
+        document.getElementById('typewriter').innerHTML = text
+
+        var char = text.slice(-1)
+        if (char === '<') isTag = true
+        if (char === '>') isTag = false
+
+        if (isTag) return type()
+        setTimeout(type, 80)
+      }())
+    }, 5000)
+  }, 5000)
 })
