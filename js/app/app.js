61be24a2ea0ece469919a2ee15a7fd50c92082b1
Rosho123 <Rosho123@users.noreply.github.com>
2017-09-17 21:18:45 -0700

    finaly there is search

--- js/app/app.js
+++ js/app/app.js
@@ -34,9 +34,11 @@ $(document).ready(function ($) {
             source: db.ref('/users/' + uid + '/docs/').orderByChild('utcdate'),
             readyCallback: function () {
               this.docs = this.fbdocs.slice().reverse()
-            //  console.log('ready', 'data')
-            //  this.docs = this.docs.slice().reverse()
-            //  console.log(this.docs)
+              this.cache = this.docs
+              console.log(vm.cache)
+              //  console.log('ready', 'data')
+              //  this.docs = this.docs.slice().reverse()
+              //  console.log(this.docs)
             }
           }
         },
@@ -44,11 +46,13 @@ $(document).ready(function ($) {
           loaded: true,
           newDocName: '',
           docs: [],
-          modalDisplay: false
+          modalDisplay: false,
+          docSearch: '',
+          cache: []
         },
 
         mounted: function () {
-        //  console.log('ready')
+          //  console.log('ready')
         },
 
         methods: {
@@ -98,7 +102,28 @@ $(document).ready(function ($) {
             } else {
               window.alert('Document Names Must Be More than 1 Character')
             }
+          },
+          search: function () {
+            var query = this.docSearch.toLowerCase()
+            let result = this.fbdocs
+            if (!query) {
+              // console.log(this.fbdocs)
+              this.docs = this.fbdocs
+              return this.fbdocs
+            }
+
+            const filterValue = query
+
+            const filter = (docs) => {
+              if (JSON.stringify(docs.title)) {
+                return JSON.stringify(docs.title).toLowerCase().includes(filterValue)
+              }
+            }
+
+            this.docs = result.filter(filter)
+            //  console.log(this.docs)
           }
+
         },
         filters: {
           reverse: function (array) {
@@ -106,31 +131,6 @@ $(document).ready(function ($) {
           }
         }
       })
-
-      function test () {
-        $('#search').on('keyup', function (acache) {
-          var query = $('#search').val().toLowerCase()
-
-          //  console.log(myImage)
-          var $pictureList = $('#docContainer a')
-          if (query) {
-            $pictureList.hide()
-
-            $pictureList.each(function () {
-              if ($(this).attr('alt').indexOf(query) >= 0) {
-                $(this).fadeIn(400, function () {
-                  // Stuff to do *after* the animation takes place
-                })
-              }
-            })
-          } else {
-            $pictureList.fadeIn(400, function () {
-              // Stuff to do *after* the animation takes place
-            })
-          }
-        })
-      }
-      test()
     }
   })
 })
