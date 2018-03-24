9f532cb86f187e664324ac870051e019e3c4feed
Ronan <rsf.sho@gmail.com>
2018-03-23 18:08:10 -0700

    fixed router link

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
       </div>
       <a class="nav-burger" href="#nav-mobile"><i class="fas fa-bars"></i></a>
       <div class="nav-menu">
-        <router-link class="minimal" to="documents">Documents</router-link>
+        <router-link class="minimal" :to="{ name: 'documents' }">Documents</router-link>
       </div>
     </div>
   </nav>
