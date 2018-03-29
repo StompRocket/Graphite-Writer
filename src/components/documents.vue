6d8461d669042e25ceed02a2140fa3b3f96547e6
Ronan <rsf.sho@gmail.com>
2018-03-28 19:03:34 -0700

    Vue Devtools Router linking

--- src/components/documents.vue
+++ src/components/documents.vue
@@ -13,24 +13,24 @@
 
     <br /> <br />
 
-    <div class="document-preview">
+    <router-link :to="{ name: 'editor', params: {document: '234fsdf'} }" class="document-preview">
       <div class="box material container">
         <h3>Document Name</h3>
         <small>
          <i>Last Edited: datetime</i>
         </small>
       </div>
-    </div>
-  </div>
 
+    </router-link>
+  </div>
   <div class="modal" id="newDocumentModal">
     <div class="modal-body box material deep">
       <div class="container">
         <i class="modal-close fas fa-times" /> <br />
         <h3>Create Document </h3>
         <span class="multi-input">
-          <input type="text" class="input" placeholder="Name Your Document" />
-          <a class="button primary input" href="#/d/">Create</a>
+          <input type="text" class="input" v-model="newDocName" placeholder="Name Your Document" @keyup.enter="newDoc"/>
+          <router-link class="button primary input" :to="{name: 'editor', params: {document: 'new', name: newDocName}}">Create</router-link>
         </span>
       </div>
     </div>
@@ -42,7 +42,20 @@ import '../assets/documents.scss'
 export default {
   name: "documents",
   data: () => ({
+    newDocName: ''
+  }),
+  methods: {
 
-  })
+    newDoc() {
+      this.$router.push({
+        name: 'editor',
+        params: {
+          document: 'new',
+          name: this.newDocName
+        }
+      })
+    }
+
+  }
 }
 </script>
