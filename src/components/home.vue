b04a12af1c0144ab24fe4b7c727a2b0074a90b46
sasha <sasha@sashas-MacBook-Air.local>
2018-04-28 08:01:09 -0700

    draft of homepage banner

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,6 +1,15 @@
 <template>
 <div class="page home">
-  <h1>Home</h1>
+  <div class="banner-image">
+  	<div class="container">
+  		<div class="center-box">
+	  		<h1 class="big"><b>Graphite Writer </b>v2</h1>
+	  		<a class="button dark" href="#!">What's New</a>
+	  		&nbsp;
+	  		<a class="button dark" href="#!">Get Started</a>
+	  	</div>
+  	</div>
+  </div>
 </div>
 </template>
 <script>
