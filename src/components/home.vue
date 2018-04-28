bdfe7e196dfc80da3bad76c3f26dc2dcdc7551a3
sasha <sasha@sashas-MacBook-Air.local>
2018-04-28 09:13:12 -0700

    military grade security

--- src/components/home.vue
+++ src/components/home.vue
@@ -10,6 +10,33 @@
 	  	</div>
   	</div>
   </div>
+
+  <div class="container white">
+    <br />
+    <br />
+    <h3>From the begenning we noticed a problem, text editors were either fast or cloud based, not both. We built Graphite Writer to solve tihs problem. Graphite Writer was designed from the ground up as a fast, easy to use, and secure text editor. All your documents are saved in the cloud, and can be accessed from anywhere. </h3>
+    <h3> At Graphite Writer we respect your privacy. That's why all your documents are encrypted with military grade security. Unlike <a href="https://www.theguardian.com/commentisfree/2014/apr/18/corporations-google-should-not-sell-customer-data" class="dark" target="_blank">some companies</a>, we believe your information is <i>yours</i>, which is why we don't sell it, or give it away without your permission.</h3>
+    <br>
+    <br>
+
+    <div class="row">
+      <div class="column center">
+        <span class="underline-warning">
+          <h1>Speed</h1>
+        </span>
+      </div>
+      <div class="column center">
+        <span class="underline-warning">
+          <h1>Usability</h1>
+        </span>
+      </div>
+      <div class="column center">
+        <span class="underline-warning">
+          <h1>Security</h1>
+        </span>
+      </div>
+    </div>
+  </div>
 </div>
 </template>
 <script>
