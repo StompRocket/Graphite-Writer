81328a044d94d33bb7a1087363891d403b176cd6
Ronan <rsf.sho@gmail.com>
2018-05-04 17:43:36 -0700

    Github Auth

--- src/components/login.vue
+++ src/components/login.vue
@@ -6,7 +6,7 @@
 	<br />
 	<div class="row">
 		<div class="column"> <a @click="loginWithGoogle" class="button warning">Google</a> </div>
-		<div class="column"> <a class="button dark">Github</a> </div>
+		<div class="column"> <a @click="loginWithGithub" class="button dark">Github</a> </div>
 	</div>
 </div>
 </div>
@@ -16,10 +16,36 @@ import "../assets/login.scss";
 import loadingScreen from "./loadingScreen.vue";
 const firebase = require("firebase");
 const provider = new firebase.auth.GoogleAuthProvider();
+const gitHub = new firebase.auth.GithubAuthProvider();
 export default {
   name: "login",
   created() {
     console.log(localStorage.getItem("redirect") + " params");
+    firebase.auth().getRedirectResult().then(function(result) {
+  if (result.credential) {
+    // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+    var token = result.credential.accessToken;
+    // ...
+  }
+  // The signed-in user info.
+  var user = result.user;
+   if (localStorage.getItem("redirect")) {
+          this.$router.push(localStorage.getItem("redirect"));
+          localStorage.removeItem("redirect", null);
+        } else {
+          this.$router.push("/documents/");
+        }
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  console.log(error)
+  // ...
+});
     firebase.auth().onAuthStateChanged(user => {
       if (user) {
         //console.log("loggedIn");
@@ -44,6 +70,9 @@ export default {
   methods: {
     loginWithGoogle() {
       firebase.auth().signInWithRedirect(provider);
+    },
+    loginWithGithub() {
+      firebase.auth().signInWithRedirect(gitHub);
     }
   }
 };
