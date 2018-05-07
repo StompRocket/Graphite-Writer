f28d4d82e35d80b4ead668c74158a226173aeba6
sasha <sasha@sashas-MacBook-Air.local>
2018-05-06 18:04:20 -0700

    video

--- src/components/v2.vue
+++ src/components/v2.vue
@@ -1,8 +1,11 @@
 <template>
 <div class="page v2">
+	<div style="padding:42.5% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/262302933" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div>
 	
+
 </div>
 </template>
+<script src="https://player.vimeo.com/api/player.js"></script>
 <script>
 import "../assets/v2.scss";
 export default {
@@ -13,3 +16,4 @@ export default {
   }
 };
 </script>
+
