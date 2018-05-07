d8456b6879ca9bce8f4e1091b7a00674c8121882
sasha <sasha@sashas-MacBook-Air.local>
2018-05-06 19:24:29 -0700

    new promo video

--- src/components/v2.vue
+++ src/components/v2.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="page v2">
-	<div style="padding:42.5% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/262302933" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div>
+	<div style="padding:42.5% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/268322197" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div>
 	
 
 </div>
