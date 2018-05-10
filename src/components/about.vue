afae88cb5b12a039719eff0a1bf02bf1ab033702
Ronan <rsf.sho@gmail.com>
2018-05-10 05:44:44 -0700

    about page

--- src/components/about.vue
+++ src/components/about.vue
@@ -1,10 +1,35 @@
 <template>
 <div class="page about">
-  <div class="container">
+   <div class="banner-image">
+  	<div class="container">
+  		<div class="center-box">
+	  		<h1 class="big" style="margin-bottom: 0; padding-bottom: 0;">Graphite Writer</h1>
+	  	
+	  	</div>
+  	</div>
+  </div>
+ 
+    <div class="row stats">
+      <div class="column ">
+        <h3>1,000+</h3>
+        <h4>Documents</h4>
+      </div>
+       <div class="column ">
+        <h3>150+</h3>
+         <h4>Users</h4>
+      </div>
+       <div class="column ">
+        <h3>3</h3>
+         <h4>Developers</h4>
+      </div>
+    </div>
+     <div class="container">
   	<br>
-  	<h1>About Us</h1>
-  	<p>Graphite Writer is a project by Ronan and Sasha S. We created Graphite Writer because we saw a problem; other web-based text editors took ages to load. In response we decided to make Graphite Writer. Our goals were to make a simple, secure, and fast web-based text editor for free. 
+  	<h3 class="sectionHeading">About Us</h3>
+  	<p class="blurb">Graphite Writer is a project by Ronan and Sasha S. We created Graphite Writer because we saw a problem; other web-based text editors took ages to load. In response we decided to make Graphite Writer. Our goals were to make a simple, secure, and fast web-based text editor for free. 
   	</p>
+    <br>
+    
   </div>
 </div>
 </template>
