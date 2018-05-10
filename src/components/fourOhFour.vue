ec8b4b53f31ad6fab023418d61c3d0959e59da94
Ronan <rsf.sho@gmail.com>
2018-05-09 22:15:43 -0700

    nicer 404

--- src/components/fourOhFour.vue
+++ src/components/fourOhFour.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="fourOhFour" class="page">
-      <h1 class="big">404</h1>
+      <h1>404</h1>
       <h2>This page could not be found</h2>
       <br>
       <router-link to="/" class="button warning">Return Home</router-link>
   </div>
 </template>
 <script>
-import "@/assets/fourOhFour.scss";
+import '@/assets/fourOhFour.scss';
 export default {
-  name: "fourOhFour",
+  name: 'fourOhFour',
   created() {
-    document.title = `Graphite Writer BETA v${this.$parent.version}  | 404`
+    document.title = `Graphite Writer BETA v${this.$parent.version}  | 404`;
   }
 };
 </script>
