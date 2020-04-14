3bb72fc4580c9e47a40be33ad87662e23e248a6c
Ronan <rsf.sho@gmail.com>
2020-04-14 16:13:54 -0700

    wip before i8n

--- deploy.js
+++ deploy.js
@@ -2,7 +2,7 @@ var sys = require('sys')
 var exec = require('child_process').exec;
 const version = require("./package.json").version
 console.log("deploying Graphite-Writer-App@v" + version)
-dir = exec(`sentry-cli releases -o stomp-rocket new "Graphite-Writer-App@${version}" -p graphite-writer && npm run build && firebase deploy &&  sentry-cli releases -o stomp-rocket finalize "Graphite-Writer-App@${version}"`, function(err, stdout, stderr) {
+dir = exec(`git add . && sentry-cli releases -o stomp-rocket new "Graphite-Writer-App@${version}" -p graphite-writer && npm run build && firebase deploy && git commit -m "Graphite-Writer-App@${version}" && git push && sentry-cli releases -o stomp-rocket finalize "Graphite-Writer-App@${version}"`, function(err, stdout, stderr) {
   if (err) {
     // should have err.code here?
     console.log(err)
