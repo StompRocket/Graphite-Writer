317ed2816ab18f777bf175d69a5fc5f972387201
Ronan <ronan@ronanfuruta.com>
2022-09-29 19:43:25 -0400

    responsifiy

--- components/gw-footer.vue
+++ components/gw-footer.vue
@@ -6,7 +6,7 @@
                 <option value="english">english</option>
             </select>
         </div>
-        <div class="flex flex-grid grid-cols-3 gap-4">
+        <div class="flex flex-grid grid-cols-3 gap-4 items-center">
             <div class="flex flex-col">
                 <h4 class="font-light">App</h4>
                 <nuxt-link>Documents</nuxt-link>
@@ -30,12 +30,12 @@
         </div>
         <div>
 <p>Graphite Writer is a free and open source project developed by Ronan Furuta</p>
-<div class="flex items-center">
+<div class="md:flex items-center">
     <p>Graphite Writer is released under the APL-2.0 License. If you’re interested in contributing to its development check out our Github</p>
-<p class="ml-auto text-right">Have a question? Contact Support.</p>
+<p class="md:ml-auto md:text-right">Have a question? Contact Support.</p>
 </div>
 <hr class="my-2">
-<div class="flex items-center">
+<div class="md:flex md:items-center">
     <p>Tagline goes here about fast and speed</p>
     <p class="ml-auto text-xs">By using Graphite Writer you consent to our use of cookies and agree to be bound by our terms of use and privacy policy</p>
 </div>
--- components/navbar.vue
+++ components/navbar.vue
@@ -1,11 +1,12 @@
 <template>
-    <nav class="flex flex-row items-center justify-left p-4 bg-back-gray grid grid-cols-12">
-        <div class="col-span-4"><img src="~/assets/images/wordmark.svg" alt="Graphite Writer Logo" class="w-72"></div>
-        <div class=" col-span-4 "><input type="search" class="gw-search w-full" placeholder="search"></div>
-        <div class="col-span-4 flex justify-end">
+    <nav class="items-center justify-left p-4 bg-back-gray grid md:grid-cols-12 md:grid-rows-1 sm:grid-rows-2 gap-4">
+        <div class="md:col-span-4 col-span-6"><img src="~/assets/images/wordmark.svg" alt="Graphite Writer Logo" class="w-72"></div>
+        <div class="md:block hidden  col-span-4 "><input type="search" class="gw-search w-full" placeholder="search"></div>
+        <div class="md:col-span-4 col-span-6 flex justify-end">
             <button class="gw-btn">NEW</button>
             <img src="~/assets/images/profile.webp" class="rounded-full w-14 h-14 ml-4 hover:opacity-80 transition-all duration-150 ease-out" alt="profile image">
         </div>
+        <div class="md:hidden block col-span-12 row-span-1 "><input type="search" class="gw-search w-full" placeholder="search"></div>
       
     </nav>
 </template>
